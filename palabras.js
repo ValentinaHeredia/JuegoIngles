d6de4a2388dc3845cd9ce08659538da9ffdd9c58
Valentina Heredia <valu.heredia05@gmail.com>
2025-08-21 15:32:31 -0300

    subo juego v1

---
+++ palabras.js
@@ -0,0 +1,35 @@
+const words = ["tough", "though", "thought", "through", "throughout", "thorough"];
+
+const elementoPalabra = document.getElementById("palabra-mostrada");
+const elementoInput = document.getElementById("ingreso-palabra");
+const elementoProximaPalabra = document.getElementById("proxima-palabra");
+
+let palabraActual;
+let proximaPalabra;
+
+function mostrarNuevaPalabra() {
+  // Si ya hay una próxima palabra, se convierte en la palabra actual
+  if (proximaPalabra) {
+    palabraActual = proximaPalabra;
+  } else {
+    // Si no, genera la primera palabra aleatoria
+    palabraActual = words[Math.floor(Math.random() * words.length)];
+  }
+
+  // Genera la próxima palabra aleatoria
+  proximaPalabra = words[Math.floor(Math.random() * words.length)];
+
+  // Muestra la palabra actual y la siguiente en sus respectivos divs
+  elementoPalabra.textContent = palabraActual;
+  elementoProximaPalabra.textContent = "Siguiente: " + proximaPalabra;
+
+  elementoInput.value = "";
+}
+
+mostrarNuevaPalabra();
+
+elementoInput.addEventListener("input", () => {
+  if (elementoInput.value.trim() === palabraActual) {
+    mostrarNuevaPalabra();
+  }
+});
